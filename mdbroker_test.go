7c5c2652b150d56e31e4decd253facd9932ed5d8
younhan.kim <younhan.kim@navercorp.com>
2018-07-24 16:39:29 +0900

    test code added

---
+++ mdbroker_test.go
@@ -0,0 +1,90 @@
+package majordomo_test
+
+import (
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	md "github.com/iamdobi/majordomo-go"
+	"github.com/iamdobi/majordomo-go/mdapi"
+)
+
+func TestPurge(t *testing.T) {
+	port := 5550
+	verbose := false
+	broker := startBroker(port, false, md.HeartbeatInterval(1500*time.Millisecond))
+
+	workerNum := 3
+	var workers []*mdapi.Mdwrk
+	for i := 0; i < workerNum; i++ {
+		workers = append(workers, startWorker(port, verbose))
+	}
+	time.Sleep(300 * time.Millisecond)
+	delWorker := workers[len(workers)-1]
+	workers = workers[0 : len(workers)-1]
+	delWorker.Close()
+
+	time.Sleep(5500 * time.Millisecond)
+
+	// broker should have only two workers
+	workerLen := broker.WorkerLen()
+	if workerLen != workerNum-1 {
+		t.Errorf("broker should have threw one worker! [%d]", workerLen)
+	}
+
+	for _, worker := range workers {
+		worker.Close()
+	}
+	broker.Close()
+}
+
+func ReqRep(t *testing.T) {
+	port := 5551
+	verbose := false
+	broker := startBroker(port, verbose)
+	worker := startWorker(port, verbose)
+
+	time.Sleep(300 * time.Millisecond)
+
+	cliapi, err := mdapi.NewMdcli(fmt.Sprintf("tcp://localhost:%d", port), verbose)
+	reply, err := cliapi.Send("echo", []byte("hello"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	replystr := string(reply[0])
+	if replystr != "hello" {
+		t.Errorf("reply should hello! [%s]", replystr)
+	}
+
+	broker.Close()
+	worker.Close()
+
+	time.Sleep(500 * time.Millisecond) // time to release
+}
+
+func startBroker(port int, verbose bool, setters ...md.Option) *md.Broker {
+	broker := md.StartBroker(port, verbose, setters...)
+	return broker
+}
+
+func startWorker(port int, verbose bool) *mdapi.Mdwrk {
+	session, _ := mdapi.NewMdwrk(fmt.Sprintf("tcp://localhost:%d", port), "echo", verbose)
+
+	go func(session *mdapi.Mdwrk) {
+		var err error
+		var request, reply [][]byte
+		for {
+			request, err = session.Recv(reply)
+			if err != nil {
+				fmt.Printf("worker interrupted..\n")
+				break //  Worker was interrupted
+			}
+			reply = request //  Echo is complex... :-)
+			log.Printf("[worker] reply=%v\n", reply)
+		}
+	}(session)
+
+	return session
+}
